42bdd2afe8aa1c3b3da96b7336b6cd511fa323d2
Ron Desmond <rhdsmnd@gmail.com>
2017-02-02 10:41:22 -0800

    Setting up structure.

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+var clicks = [];
+
+app.listen(80, function() {
+	console.log("Web server started.");
+});
--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,7 +2,52 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+var ip = '127.0.0.1';
+
 class App extends Component {
+  
+	constructor() {
+		super();
+		this.state = {
+			interval: 'd',
+			date: new Date(),
+			sessions: []
+		};
+	}
+
+	componentWillMount() {
+		var req = new XMLHttpRequest();
+
+		var url = "query?interval=" + this.state.interval + "&"
+					+ "date=" + this.state.date;
+
+		req.open("GET", "http://localhost/" + url);
+
+		req.addEventListener("load", function(e) {
+			var resp = e.target.response;
+
+			this.setState({
+				sessions: resp.sessions
+			});
+		});
+	}
+
+
+
+	render() {
+		return (
+			<div>
+				<AppHeader date={this.state.date} interval={this.state.interval} sessions={this.state.sessions} />
+
+				<hr />
+
+				<AppBody date={this.state.date} interval={this.state.interval} sessions={this.state.sessions} />
+			</div>	
+		);
+	}
+
+	/**
   render() {
     return (
       <div className="App">
@@ -15,7 +60,74 @@ class App extends Component {
         </p>
       </div>
     );
-  }
+  }*/
 }
+
+	/**
+class DateSelector extends Component {
+	constructor(props) {
+
+	}
+}
+*/
+
+
+class AppHeader extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>placeholder header text</div>
+		);
+	}
+
+}
+
+	/**
+class DataSummary extends Component {
+
+	constructor(props) {
+
+	}
+}
+*/
+
+class AppBody extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>placeholder body text</div>
+		);
+	}
+}
+	/**
+class StatsTable extends Component {
+
+	constructor(props) {
+
+	}
+}
+
+class Timeline extends Component {
+
+	constructor(props) {
+
+	}
+}
+
+class PieChart extends Component {
+
+	constructor(props) {
+
+	}
+}
+*/
 
 export default App;
